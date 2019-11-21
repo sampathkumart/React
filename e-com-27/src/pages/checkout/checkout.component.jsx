336d4d14c321e730ab84c2d61f2aa6857ea1570a
sampathkumar <semsampathkumar@gmail.com>
2019-11-21 20:36:41 +0530

    checkout page

---
+++ e-com-27/src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => (
+    <React.Fragment>
+        CHECKOUT PAGE
+     </React.Fragment>
+)
+
+export default CheckoutPage;
