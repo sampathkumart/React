cba3b8fe23e3cee224d3b82a93d61d38d78c1d7b
sampathkumar <semsampathkumar@gmail.com>
2019-11-23 18:54:26 +0530

    nested routing in shop page

---
+++ e-com-34/src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ( match) => {
+   console.log(match);
+    return(
+    <div className='colection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+)};
+
+export default CollectionPage;
---
+++ e-com-34/src/pages/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import  CollectionsOverview  from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => (
+      <div className='shop-page'>
+          <Route exact path={`${match.path}`} component ={CollectionsOverview} />
+          <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+      </div>
+    );
+
+
+export default ShopPage;
