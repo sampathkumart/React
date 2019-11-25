3d034b8ff0c35e649840abbe29ea3648f2528c98
sampathkumar <semsampathkumar@gmail.com>
2019-11-25 21:45:53 +0530

    updating favicon and app title

---
+++ e-com-37/src/components/homepage/homepage.component.jsx
@@ -0,0 +1,47 @@
+/*import React from 'react';
+
+import './homepage.styles.scss';
+ 
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>samsung</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>nokia</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>redmi</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>lava</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>lenovo</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+        </div >
+    </div >
+
+);
+
+export default HomePage;*/
---
+++ e-com-37/src/components/stripe-button/stripe.button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_g0b36AO5QOoL03cF8CDRg36W00FS1UgrLv';
+  
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
